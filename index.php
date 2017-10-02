ebf4cbd972f122afce45b146c400080a4a00e7fa
yesninja <aaron.lamar@yahoo.com>
2017-10-01 23:45:43 -0700

    prettify

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 <title>Horror Helper</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="www/css/app.css">
 
 </head>
 
@@ -33,80 +35,5 @@
 </body>
 
 <script type="text/javascript" src="www/js/app.js"></script>
-
-<script type="text/javascript">
-
-var app_url = "app.php";
-
-$( document ).ready(function() {
-	
-	var data = {
-		"c":"Movie",
-		"m":"getCurrentMovie"
-	};
-
-	$.post( app_url, data)
-	  .done(function( data ) {
-	    displayMovie(data);
-	});
-
-	// Clicks...
-	$( "#watched_it" ).on("click", function() {
-		var data = {
-			"c":"Movie",
-			"m":"getNextMovie",
-			"watched":true
-		};
-
-		$.post( app_url, data)
-		  .done(function( data ) {
-		    displayMovie(data);
-		});		  
-	});
-
-	$( "#skip_it" ).on("click", function() {
-		var data = {
-			"c":"Movie",
-			"m":"getNextMovie",
-			"skip":true
-		};
-		
-		$.post( app_url, data)
-		  .done(function( data ) {
-			displayMovie(data);
-		});		  
-	});
-});
-
-function displayMovie(obj)
-{
-	$("#title").html(obj.title);
-    $("#poster").attr("src","https://image.tmdb.org/t/p/w640/"+obj.poster_path);
-    if (obj.trailer_link) {
-    	$("#trailer").html("<iframe width='420' height='315' src='https://www.youtube.com/embed/"+obj.trailer_link+"'></iframe>");
-    } else {
-    	$("#trailer").html("");
-    }
-    if (obj.imdb_rating || obj.imdb_id) {
-    	var imdb_html = obj.imdb_rating;
-    	if (obj.imdb_id)
-    	{
-    		 imdb_html = "<a href='http://www.imdb.com/title/"+obj.imdb_id+"' target='_blank'>"+obj.imdb_rating+"</a>";
-    	}
-    	else
-    	{
-    		imdb_html = "<a href='http://www.imdb.com/title/"+obj.imdb_id+"' target='_blank'>"+obj.imdb_rating+"</a>";
-    	}
-
-    	$("#imdb").html(imdb_html);
-    } else {
-    	$("#imdb").html("No IMDB data :(");
-    }
-    $("#overview").html(obj.overview);
-    $("#release").html("Released: "+obj.release_date);
-    $("#language").html("Language: "+obj.language);
-}
-
-</script>
 </html>
 
--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,70 @@
+var app_url = "app.php";
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+	
+	var data = {
+		"c":"Movie",
+		"m":"getCurrentMovie"
+	};
+
+	$.post( app_url, data)
+	  .done(function( data ) {
+	    displayMovie(data);
+	});
+
+	// Clicks...
+	$( "#watched_it" ).on("click", function() {
+		var data = {
+			"c":"Movie",
+			"m":"getNextMovie",
+			"watched":true
+		};
+
+		$.post( app_url, data)
+		  .done(function( data ) {
+		    displayMovie(data);
+		});		  
+	});
+
+	$( "#skip_it" ).on("click", function() {
+		var data = {
+			"c":"Movie",
+			"m":"getNextMovie",
+			"skip":true
+		};
+		
+		$.post( app_url, data)
+		  .done(function( data ) {
+			displayMovie(data);
+		});		  
+	});
 });
+
+function displayMovie(obj)
+{
+	$("#title").html(obj.title);
+    $("#poster").attr("src","https://image.tmdb.org/t/p/w640/"+obj.poster_path);
+    if (obj.trailer_link) {
+    	$("#trailer").html("<iframe width='420' height='315' src='https://www.youtube.com/embed/"+obj.trailer_link+"'></iframe>");
+    } else {
+    	$("#trailer").html("");
+    }
+    if (obj.imdb_rating || obj.imdb_id) {
+    	var imdb_html = obj.imdb_rating;
+    	if (obj.imdb_id)
+    	{
+    		 imdb_html = "<a href='http://www.imdb.com/title/"+obj.imdb_id+"' target='_blank'>"+obj.imdb_rating+"</a>";
+    	}
+    	else
+    	{
+    		imdb_html = "<a href='http://www.imdb.com/title/"+obj.imdb_id+"' target='_blank'>"+obj.imdb_rating+"</a>";
+    	}
+
+    	$("#imdb").html(imdb_html);
+    } else {
+    	$("#imdb").html("No IMDB data :(");
+    }
+    $("#overview").html(obj.overview);
+    $("#release").html("Released: "+obj.release_date);
+    $("#language").html("Language: "+obj.language);
+}
